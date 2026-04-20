0e91abed7d2e7fb0b5902e1f00cc34602377131c
Nahramoss <ramosnatalia876@gmail.com>
2026-04-20 18:19:53 -0300

    Projeto iniciado

---
+++ scripts.js
@@ -0,0 +1,36 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#ee4fcc" }, /* Mudado para o rosa do seu tema para ficar visível */
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ee4fcc", /* Mudado para rosa */
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" }
+    },
+    modes: {
+      grab: { distance: 140, line_linked: { opacity: 1 } },
+      push: { particles_nb: 4 }
+    }
+  },
+  retina_detect: true
+});
